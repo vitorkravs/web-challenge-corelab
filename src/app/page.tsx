966f6de41188cfa09ee64d0ae6bb5c29f3e9454e
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-15 00:19:39 -0300

    feat: adicionando o componente CreateNote na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import CreateNote from "@/components/CreateNote/CreateNote";
 import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main>
+        <CreateNote />
+      </main>
     </>
   );
 }
