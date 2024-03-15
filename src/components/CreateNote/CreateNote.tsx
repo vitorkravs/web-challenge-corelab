4b99c654aaef90370587d218147aa057abddfe7a
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-15 14:00:52 -0300

    feat: acessando api feita no backend, para criação de novas anotações(post/create)

--- src/components/CreateNote/CreateNote.tsx
+++ src/components/CreateNote/CreateNote.tsx
@@ -1,24 +1,90 @@
+"use client";
+
 //styles
-import { IoMdStarOutline } from "react-icons/io";
 import "./styles.scss";
 
+//hooks
+import { useState } from "react";
+
+//requisição
+import axios from "axios";
+
 //icons
+import { IoMdStarOutline } from "react-icons/io";
+import { LiaStarSolid } from "react-icons/lia";
 
 const CreateNote = () => {
+  const [title, setTitle] = useState("");
+  const [annotation, setAnnotation] = useState("");
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const createNewNote = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios
+        .post("http://localhost:3333/api/annotations/post", {
+          title: title,
+          annotation: annotation,
+          is_favorite: isFavorite,
+        })
+        .then(function (response) {
+          console.log(response);
+        });
+      setTitle("");
+      setAnnotation("");
+      setIsFavorite(false);
+    } catch (error) {
+      console.log("erro ao criar nota, ", error);
+    }
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleAnnotationChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setAnnotation(e.target.value);
+  };
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+    console.log(isFavorite);
+  };
+
   return (
     <div id="create-note">
-      <form id="create-note-form">
+      <form id="create-note-form" onSubmit={createNewNote}>
         <div id="title-is-favorive">
-          <input type="text" placeholder="Título" />
+          <input
+            type="text"
+            placeholder="Título"
+            onChange={handleTitleChange}
+            value={title}
+            required
+          />
           <div id="is-favorite-btn">
-            <IoMdStarOutline className="toggle-favorite" />
+            {isFavorite ? (
+              <LiaStarSolid className="favorite" onClick={toggleFavorite} />
+            ) : (
+              <IoMdStarOutline className="normal" onClick={toggleFavorite} />
+            )}
           </div>
         </div>
         <textarea
           name="annotation"
-          id="create-annotation-input"
+          id="create-annotation-textarea"
           placeholder="Criar nota..."
+          onChange={handleAnnotationChange}
+          value={annotation}
+          required
         ></textarea>
+        <div id="create-note-btn-container">
+          <button type="submit" id="create-note-btn">
+            Criar
+          </button>
+        </div>
       </form>
     </div>
   );
