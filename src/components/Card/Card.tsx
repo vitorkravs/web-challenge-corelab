74e6528b8f9744e36b86f3a0cfb751ced75409ea
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-15 22:54:24 -0300

    feat: criando a estrutura do componente Card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,25 @@
+import { IoMdStarOutline } from "react-icons/io";
+import { LiaStarSolid } from "react-icons/lia";
+
+interface CardProps {
+  title: string;
+  annotation: string;
+  isFavorite?: boolean;
+}
+
+const Card = ({ title, annotation, isFavorite }: CardProps) => {
+  console.log(title, annotation, isFavorite);
+  return (
+    <div className="favorite-notes">
+      <form>
+        <div>
+          <input value={title} readOnly />
+          <div>{isFavorite ? <LiaStarSolid /> : <IoMdStarOutline />}</div>
+        </div>
+        <textarea value={annotation} readOnly></textarea>
+      </form>
+    </div>
+  );
+};
+
+export default Card;
