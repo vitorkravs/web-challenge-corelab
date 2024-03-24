ff3525541ea79a03523b0d752247c58d35e5bc0f
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-23 22:23:10 -0300

    feat: refazendo estrutura do componente

--- src/components/FavoriteNotes/FavoriteNotes.tsx
+++ src/components/FavoriteNotes/FavoriteNotes.tsx
@@ -1,17 +1,7 @@
-"use client";
-
 //styles
 import "./styles.scss";
 
-//icons
-import { LiaStarSolid } from "react-icons/lia";
-import { IoMdStarOutline } from "react-icons/io";
-
-//hooks
-import { useState, useEffect } from "react";
-
-//requisição
-import axios from "axios";
+//components
 import Card from "../Card/Card";
 
 interface Note {
@@ -21,30 +11,15 @@ interface Note {
   isFavorite: boolean;
 }
 
-const FavoriteNotes = () => {
-  const [notes, setNotes] = useState<Note[]>([]);
+interface FavoriteNotesProps {
+  favoriteNotes: Note[];
+}
 
-  useEffect(() => {
-    const getNotes = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:3333/api/annotations"
-        );
-        setNotes(response.data);
-        console.log(response);
-      } catch (error) {
-        console.log("erro ao criar nota, ", error);
-      }
-    };
-    getNotes();
-  }, []);
-
-  const favoriteNotes = notes.filter((note) => note.isFavorite);
-
+const FavoriteNotes = ({ favoriteNotes }: FavoriteNotesProps) => {
   return (
     <div id="favorite-notes-container">
       <h2 id="title-favorite-notes">Favoritas</h2>
-      {notes.map((note) => (
+      {favoriteNotes.map((note) => (
         <Card
           key={note.id}
           title={note.title}
