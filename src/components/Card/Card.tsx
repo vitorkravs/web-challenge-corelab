dcf9439dac3b708555d313327ed2f015f56c02b0
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-25 17:25:27 -0300

    feat: atualizando estrutura do componente e utilizando o as variáveis do context

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,24 +1,83 @@
+"use client";
+
+//styles
 import "./styles.scss";
 
-import { IoMdStarOutline } from "react-icons/io";
+//icons
+import { FaPencilAlt } from "react-icons/fa";
+import { IoMdClose, IoMdStarOutline } from "react-icons/io";
 import { LiaStarSolid } from "react-icons/lia";
+import { LuPaintBucket } from "react-icons/lu";
+
+//hooks
+import { useState } from "react";
+
+//context
+import { useNotes } from "@/context/notesContext";
 
 interface CardProps {
+  id: string;
   title: string;
   annotation: string;
-  isFavorite?: boolean;
+  isFavorite: boolean;
 }
 
-const Card = ({ title, annotation, isFavorite }: CardProps) => {
-  console.log(title, annotation, isFavorite);
+const Card = ({
+  id,
+  title: initialTitle,
+  annotation: initialAnnotation,
+  isFavorite: initialIsFavorite,
+}: CardProps) => {
+  const { updateNote, updateIsFavorite } = useNotes();
+  const [title, setTitle] = useState(initialTitle);
+  const [annotation, setAnnotation] = useState(initialAnnotation);
+  const [isFavorite, setIsFavorite] = useState(initialIsFavorite);
+
+  const handleSave = async () => {
+    await updateNote({ id, title, annotation, isFavorite });
+  };
+
+  const handleIsFavorite = async () => {
+    await updateIsFavorite({ id, isFavorite: !isFavorite });
+    setIsFavorite(!isFavorite);
+  };
   return (
     <div className="card-notes">
-      <form>
-        <div>
-          <input value={title} readOnly />
-          <div>{isFavorite ? <LiaStarSolid /> : <IoMdStarOutline />}</div>
+      <form className="form-card" onSubmit={(e) => e.preventDefault()}>
+        <div className="title-is-favorite-card">
+          <input
+            className="title-input-card"
+            defaultValue={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className="is-favorite-icon-card" onClick={handleIsFavorite}>
+            {isFavorite ? (
+              <LiaStarSolid className="favorite-note" />
+            ) : (
+              <IoMdStarOutline className="normal-note" />
+            )}
+          </div>
         </div>
-        <textarea value={annotation} readOnly></textarea>
+        <textarea
+          className="textarea-card"
+          defaultValue={annotation}
+          onChange={(e) => setAnnotation(e.target.value)}
+        ></textarea>
+        <div className="edit-card">
+          <div className="edit-and-color">
+            <button className="edit-btn-card" onClick={handleSave}>
+              <FaPencilAlt />
+            </button>
+            <button className="edit-color-card">
+              <LuPaintBucket />
+            </button>
+          </div>
+          <div className="remove-card-btn">
+            <button className="remove-card">
+              <IoMdClose />
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
