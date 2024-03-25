ab183f72fb5f48f4956294bafb701d5de4836e9f
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-25 17:23:35 -0300

    feat: utilizando context global na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import CreateNote from "@/components/CreateNote/CreateNote";
 import Header from "@/components/Header/Header";
 import ShowNotes from "@/components/ShowNotes/ShowNotes";
+import { NotesProvider } from "@/context/notesContext";
 
 export default function Home() {
   return (
-    <>
+    <NotesProvider>
       <Header />
       <main>
         <CreateNote />
         <ShowNotes />
       </main>
-    </>
+    </NotesProvider>
   );
 }
