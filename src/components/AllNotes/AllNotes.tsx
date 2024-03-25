d54266bdb3c33b8f74b39b6fe690074800dd407a
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-25 17:24:09 -0300

    feat: Criando estrutura do componente AllNotes

---
+++ src/components/AllNotes/AllNotes.tsx
@@ -0,0 +1,35 @@
+//styles
+import "./styles.scss";
+
+//components
+import Card from "../Card/Card";
+
+interface Note {
+  id: string;
+  title: string;
+  annotation: string;
+  isFavorite: boolean;
+}
+
+interface AllNotesProps {
+  notes: Note[];
+}
+
+const AllNotes = ({ notes }: AllNotesProps) => {
+  return (
+    <div id="all-notes-container">
+      <h2 id="title-all-notes">Outras</h2>
+      {notes.map((note) => (
+        <Card
+          key={note.id}
+          id={note.id}
+          title={note.title}
+          annotation={note.annotation}
+          isFavorite={note.isFavorite}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default AllNotes;
